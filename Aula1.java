8c57d4d95667460d4bd35564153173e901fa4bb2
IgorMiranda56 <159519511+IgorMiranda56@users.noreply.github.com>
2024-07-30 11:20:17 -0300

    Add files via upload

---
+++ Aula1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula1;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Aula1 {
+
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        String nome = "Marta";
+        int idade = 30;
+        double peso = 65.2;
+        
+        // Declaração de constante
+        final String CPF = "123.456.789-80";
+        
+        //Exibindo resultados
+        //Concatenando com o sinal de "+"         
+        //sout (tab)
+        System.out.println("o nome do usuário é: " + nome);
+        System.out.println("O CPF do usuário é: " + CPF);
+        System.out.println("A idade do usuário é: " + idade + " anos");
+        System.out.println("O peso do usuário è: " + peso + " quilos");
+    }
+}
