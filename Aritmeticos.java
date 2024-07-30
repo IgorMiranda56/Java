b73260d97ad364857a56c02ad85505f89002c032
IgorMiranda56 <159519511+IgorMiranda56@users.noreply.github.com>
2024-07-30 11:20:45 -0300

    Add files via upload

---
+++ Aritmeticos.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aritmeticos;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Aritmeticos {
+
+    public static void main(String[] args) {
+        //Declarando a variavel
+        int a = 10;
+        int b = 2;
+        
+        //Operadores aritmeticos
+        int multiplicar = a * b;
+        int dividir = a / b;
+        int somar = a + b;
+        int subtrair = a - b;
+        
+        //Exibindo resultados
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+    }
+}
