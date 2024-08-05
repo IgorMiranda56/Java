b44defa343c1014c30f9c8afce666b355c0df05f
IgorMiranda56 <159519511+IgorMiranda56@users.noreply.github.com>
2024-08-05 07:53:54 -0300

    Add files via upload

---
+++ Java1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java1;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Java1 {
+
+    public static void main(String[] args) {
+        //Declarando variáveis.
+        int a = 10;
+        int b = 2;
+        int maior;
+        
+        if (a > b){
+            maior = a;
+        }else {
+            maior = b;
+        }
+        
+        //Exibindo resultados.
+        System.out.println("O maior número é: " + maior);
+    }
+}
